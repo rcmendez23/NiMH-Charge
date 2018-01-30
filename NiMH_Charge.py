817100350cfc5fea393699a678364136a73f6fcb
rmendez23 <rmendez2017@spyponders.com>
2018-01-30 13:54:34 -0500

    Added print statements

--- NiMH_Charge.py
+++ NiMH_Charge.py
@@ -7,7 +7,11 @@ PIN12= "P9_12"
 GPIO.setup(PIN12, GPIO.OUT)
 while True:
 	GPIO.output(PIN12, GPIO.LOW)
+	print("OFF...")
 	time.sleep(5000)
+	print("delay...")
 	GPIO.output(PIN12, GPIO.HIGH)
+	print("ON...")
 	time.sleep(5000)
+	print("delay...")
 #GPIO.cleanup() #cleans up pins
