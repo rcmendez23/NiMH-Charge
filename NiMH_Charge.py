aafde79b110e2427be16bbd62f8ad354e88ccb18
rmendez23 <rmendez2017@spyponders.com>
2018-01-30 13:29:27 -0500

    added while(true) loop and changed sleep(1000) to 5000

--- NiMH_Charge.py
+++ NiMH_Charge.py
@@ -3,10 +3,9 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 GPIO.setup("P9_14", GPIO.OUT)
-#for i in range(5):
-GPIO.output("P9_14", GPIO.LOW)
-time.sleep(1000)
-#GPIO.output("P9_14", GPIO.HIGH)
-
+while(true):
+	GPIO.output("P9_14", GPIO.LOW)
+	time.sleep(5000)
+	GPIO.output("P9_14", GPIO.HIGH)
 
 #GPIO.cleanup() #cleans up pins
