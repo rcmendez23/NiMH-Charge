9c5dfff1d720e963e8c130c853ea1f4be2aae039
rmendez23 <rmendez2017@spyponders.com>
2018-01-30 13:33:59 -0500

    Added while True
    True needs to be upper case

--- NiMH_Charge.py
+++ NiMH_Charge.py
@@ -3,7 +3,7 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 GPIO.setup("P9_14", GPIO.OUT)
-while(true):
+while True:
 	GPIO.output("P9_14", GPIO.LOW)
 	time.sleep(5000)
 	GPIO.output("P9_14", GPIO.HIGH)
