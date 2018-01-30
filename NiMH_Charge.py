179a55619e33fd2b86c0bf0ca85add32528623af
rmendez23 <rmendez2017@spyponders.com>
2018-01-30 13:57:37 -0500

    Changed time.sleep(5000) to time.sleep(5)
    Because it's in seconds not miliseconds oops

--- NiMH_Charge.py
+++ NiMH_Charge.py
@@ -8,10 +8,10 @@ GPIO.setup(PIN12, GPIO.OUT)
 while True:
 	GPIO.output(PIN12, GPIO.LOW)
 	print("OFF...")
-	time.sleep(5000)
+	time.sleep(5)
 	print("delay...")
 	GPIO.output(PIN12, GPIO.HIGH)
 	print("ON...")
-	time.sleep(5000)
+	time.sleep(5)
 	print("delay...")
 #GPIO.cleanup() #cleans up pins
