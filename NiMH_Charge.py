3eca4bbd709d9108c6b06177609d7dd8eb04f469
rmendez23 <rmendez2017@spyponders.com>
2018-02-01 14:33:35 -0500

    Relay program to turn on relays 2 and 3
    Should light up LEDs

--- NiMH_Charge.py
+++ NiMH_Charge.py
@@ -2,10 +2,24 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-PIN12= "P9_12"
+PIN12= "P9_12" #GPIO60
+RELAY2 = "P9_11" #GPIO30
+RELAY3 = "P9_13" #GPIO31
 
 GPIO.setup(PIN12, GPIO.OUT)
+GPIO.setup(RELAY2, GPIO.OUT)
+GPIO.setup(RELAY3, GPIO.OUT)
 while True:
+	print("Relay2 ON")
+	GPIO.output(RELAY2, GPIO.HIGH)
+	GPIO.output(RELAY3, GPIO.LOW)
+	time.sleep(2) #Wait 2 sec
+	print("Relay3 ON")
+	GPIO.output(RELAY2, GPIO.LOW)
+	GPIO.output(RELAY3, GPIO.HIGH)
+	time.sleep(2) #Wait 2 sec
+	
+	'''
 	print("OFF...")
 	GPIO.output(PIN12, GPIO.LOW)
 	print("delay...")
@@ -14,4 +28,6 @@ while True:
 	GPIO.output(PIN12, GPIO.HIGH)
 	print("delay...")
 	time.sleep(5)
+	'''
 #GPIO.cleanup() #cleans up pins
+
