79ec93075f6b1498f37f3a90849ee88c174bdff2
rmendez23 <rmendez2017@spyponders.com>
2018-02-05 13:00:25 -0500

    Added print statements
    only printing the adc raw data

--- NiMH_Charge.py
+++ NiMH_Charge.py
@@ -1,33 +1,30 @@
 #example
 import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.ADC as ADC
 import time
 
 PIN12= "P9_12" #GPIO60
+RELAY1 = "P9_17" #GPIO4
 RELAY2 = "P9_11" #GPIO30
 RELAY3 = "P9_13" #GPIO31
+ADC1 = "P9_33" #AIN4
+adc_data = 0.00
+voltage = 0.00
 
-GPIO.setup(PIN12, GPIO.OUT)
+GPIO.setup(RELAY1, GPIO.OUT)
 GPIO.setup(RELAY2, GPIO.OUT)
 GPIO.setup(RELAY3, GPIO.OUT)
+ADC.setup()
+
 while True:
-	print("Relay2 ON")
-	GPIO.output(RELAY2, GPIO.HIGH)
-	GPIO.output(RELAY3, GPIO.LOW)
+	print("Relay1 ON")
+	GPIO.output(RELAY1, GPIO.HIGH) 
 	time.sleep(2) #Wait 2 sec
-	print("Relay3 ON")
-	GPIO.output(RELAY2, GPIO.LOW)
-	GPIO.output(RELAY3, GPIO.HIGH)
-	time.sleep(2) #Wait 2 sec
-	
-	'''
-	print("OFF...")
-	GPIO.output(PIN12, GPIO.LOW)
-	print("delay...")
-	time.sleep(5)
-	print("ON...")
-	GPIO.output(PIN12, GPIO.HIGH)
-	print("delay...")
-	time.sleep(5)
-	'''
+	adc_data = ADC.read(ADC1)
+	voltage = adc_data * 1.8 #1.8V
+	#print("Voltage: ")
+	#print(voltage)
+	print("adc_data: " + adc_data)
+
 #GPIO.cleanup() #cleans up pins
 
