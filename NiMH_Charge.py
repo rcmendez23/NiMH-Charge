ce6995886791a5ed1b41c866ade200df9cf67305
Rebecca Mendez <mendez.r@husky.neu.edu>
2018-02-12 15:44:27 -0500

    fixed time_elapsed printing as zero
    made time.sleep(5) again for testing purposes

--- NiMH_Charge.py
+++ NiMH_Charge.py
@@ -13,7 +13,7 @@ ADC1 = "P9_33" #AIN4
 adc_data = 0.00 #raw adc pin data
 #global voltage = 0.00 #voltage of battery
 i = 0 #counter
-time_elapsed = 0 #run time of program, how long battery takes to get to 10V
+global time_elapsed = 0 #run time of program, how long battery takes to get to 10V
 email = "nimh.charge@gmail.com" #email to send and recieve notification
 pwd = "bu0y$0Lar7"  
 
@@ -64,12 +64,12 @@ while True:
 	if voltage <= 10.0:
 		relayOff(RELAY1) #Turn off relay 1 if the voltage goes down to 10V
 		print("Relay OFF...Reached 10V")
-		print("Time Elapsed: " + str(time_elapsed)) 
 		time_elapsed = timeit.timeit() #keep track of time elapsed
+		print("Time Elapsed: " + str(time_elapsed)) 
 		notification(time_elapsed) #Send email notification
 		v_datafile.close() #close data file
 		break
-	time.sleep(300) #take voltage reading every 5 minutes
+	time.sleep(5) #take voltage reading every 5 minutes
 exit() #exit program
 #GPIO.cleanup() #cleans up pins
 
