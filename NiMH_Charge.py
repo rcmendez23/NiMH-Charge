8f9d69401f75934be53a8c8fa28236a5944bf70d
Rebecca Mendez <mendez.r@husky.neu.edu>
2018-02-13 14:18:51 -0500

    Fixing issue of not writing to data file
    made v_datafile an input to the function write_Data()

--- NiMH_Charge.py
+++ NiMH_Charge.py
@@ -10,7 +10,7 @@ RELAY1 = "P9_17" #GPIO4
 RELAY2 = "P9_11" #GPIO30
 RELAY3 = "P9_13" #GPIO31
 ADC1 = "P9_33" #AIN4
-adc_data = 0.00 #raw adc pin data
+#adc_data = 0.00 #raw adc pin data
 #global voltage = 0.00 #voltage of battery
 i = 0 #counter
 global time_elapsed #run time of program, how long battery takes to get to 10V
@@ -43,7 +43,7 @@ def calc_Voltage():
 	voltage = (adc_data * 1.8*9)*(12.59/12.78) #convert to volts
 	#return voltage
 
-def write_Data(voltage):
+def write_Data(v_datafile):
 	v_datafile.write(str(voltage)) #write voltages to data file
 
 #Send Email	
@@ -60,6 +60,7 @@ def notification(time_elapsed):
 while True:
 	relayOn(RELAY1) #Turn on relay 1
 	calc_Voltage() #compute voltage, print it, send it to text file
+	write_Data(v_datafile) #write data to voltage_data.txt
 	print("Voltage: " + str(voltage)) #print out voltage to user
 	if voltage <= 10.0:
 		relayOff(RELAY1) #Turn off relay 1 if the voltage goes down to 10V
